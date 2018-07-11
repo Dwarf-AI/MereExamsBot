8b08628c6e6fbffa82a5311c90e5d3c2d1dd9a6f
Deepak <dmangla3@gmail.com>
2018-07-11 22:30:30 +0530

    Designed basic structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import json
 from flask import Flask, request, make_response, jsonify
+
 app = Flask(__name__)
 log = app.logger
 
@@ -9,32 +10,32 @@ def course_detail(sess):
     """
     Return Course_details if Course and Specialization known.
     """
+    if 'Course_Name' in sess.keys() and 'Specialization' in sess.keys() and 'Course_detail' in sess.keys():
+        return make_response(jsonify({'fulfillmentText': f'{sess["Course_detail"]} of {sess["Course_Name"]} in {sess["Specialization"]} in {sess["CollegeName"]} is answer'}))
 
-    if 'Course_Name' in sess.keys() and 'Specialization' in sess.keys() and 'College_detail' in sess.keys():
-        return make_response(jsonify({'fulfillmentText': f'{sess["College_detail"]} of {sess["Course_Name"]} in {sess["Specialization"]} in {sess["CollegeName"]} is answer'}))
-
-    elif 'Course_Name' in sess.keys() and 'Specialization' in sess.keys() and 'College_detail' not in sess.keys():
+    elif 'Course_Name' in sess.keys() and 'Specialization' in sess.keys() and 'Course_detail' not in sess.keys():
         return make_response(jsonify({'fulfillmentText': f'What you want to know about {sess["Specialization"]} in {sess["Course_Name"]}'}))
 
-    elif 'Course_Name' in sess.keys() and 'Specialization' not in sess.keys() and 'College_detail' in sess.keys():
+    elif 'Course_Name' in sess.keys() and 'Specialization' not in sess.keys() and 'Course_detail' in sess.keys():
         return make_response(jsonify({'fulfillmentText': f'Choose specialization in {sess["Course_Name"]}'}))
 
-    elif 'Course_Name' not in sess.keys() and 'Specialization' in sess.keys() and 'College_detail' in sess.keys():
+    elif 'Course_Name' not in sess.keys() and 'Specialization' in sess.keys() and 'Course_detail' in sess.keys():
         return make_response(jsonify({'fulfillmentText': f'Choose course in {sess["Specialization"]}'}))
 
-    elif 'Course_Name' not in sess.keys() and 'Specialization' not in sess.keys() and 'College_detail' in sess.keys():
-        return make_response(jsonify({'fulfillmentText': f'Select a course whose {sess["College_detail"]} you want to know'}))
-    elif 'Course_Name' in sess.keys() and 'Specialization' not in sess.keys() and 'College_detail' not in sess.keys():
+    elif 'Course_Name' not in sess.keys() and 'Specialization' not in sess.keys() and 'Course_detail' in sess.keys():
+        return make_response(jsonify({'fulfillmentText': f'Select a course whose {sess["Course_detail"]} you want to know'}))
+    elif 'Course_Name' in sess.keys() and 'Specialization' not in sess.keys() and 'Course_detail' not in sess.keys():
         return make_response(jsonify({'fulfillmentText': f'Select a specialization in {sess["Course_Name"]}'}))
     else:
-        return make_response(jsonify({'fulfillment':'Not found'}))
+        return None
 
-
-def course_list(sess):
+def clg_list(sess):
+    print('ok')
     if 'UniversityName' in sess.keys():
         return make_response(jsonify({'fulfillmentText': f'Select a college in {sess["UniversityName"]}'}))
 
     elif 'Course_Name' in sess.keys():
+        print('hmm')
         return make_response(jsonify({'fulfillmentText': f'Select Universities offering {sess["Course_Name"]}'}))
 
     elif 'CityName' in sess.keys():
@@ -50,6 +51,12 @@ def course_list(sess):
         return make_response(jsonify({'fulfillmentText': f'Select Select Course you wanna take in {sess["Ownership"]}'}))
     else:
         return make_response(jsonify({'fulfillment':'Not found'}))
+
+def college_detail(sess):
+    if 'CollegeName' in sess.keys():
+        return 'About college, Admission,contact info'
+    else:
+        return 'Select a college'
 
 @app.route('/', methods=["POST"])
 def webhook():
@@ -68,33 +75,43 @@ def webhook():
 
 # Retrieve parameters and store in session_id of user.
     params_update = req["queryResult"]["parameters"].keys()
+    #print(params_update.values())
     for param in params_update:
-        print(req["queryResult"]["parameters"][param])
+        if req['queryResult']['parameters'][param] == '':
+            continue
+        #print(req["queryResult"]["parameters"][param])
         if req["session"] in user_sessions.keys():
             user_sessions[req["session"]][param] = req["queryResult"]["parameters"][param]
         else:
             user_sessions[req["session"]] = defaultdict()
+            user_sessions[req['session']][param] = req['queryResult']['parameters'][param]
         sess = user_sessions[req["session"]]     #shortcut to access parameters
-
+    print(action)
     if action == 'College_info':
         if 'CollegeName' in sess.keys():
             if 'College_detail' in sess.keys():
                 if sess["College_detail"] == 'Course_info':
-                    course_detail(sess)
+                    answer = course_detail(sess)
+                    if answer == None:
+                        return make_response(jsonify({'fulfillment': f'all courses in {sess["CollegeName"]} '}))
+                    else:
+                        return answer
                 else:
-                    return make_response(jsonify({'fulfillmentText': f'What you want to know : about, admission, contact'}))
-
+                    return make_response(jsonify({'fulfillmentText': f'{sess["College_detail"]} '
+                                                                     f'for {sess["CollegeName"]} is'
+                                                                     f''}))
             answer = course_detail(sess)
             if answer == None:
-                return make_response(jsonify({'fulfillmentText': f'Select a college'}))
+                return make_response(jsonify({'fulfillmentText': f'About college, Admission,contact info'}))
             else:
-                return make_response(jsonify({'fulfillmentText': f'{answer}'}))
+                return answer #make_response(jsonify({'fulfillmentText': f'{answer}'}))
 
         else:
-            course_list(sess)
+            return clg_list(sess)
 
     if action == 'list':
-        course_list(sess)
+        print('list')
+        return clg_list(sess)
 
 
 if __name__ == '__main__':
