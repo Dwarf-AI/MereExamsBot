d739c31140bf36d36c94898d21d578834a08e1a0
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2018-08-04 12:42:08 +0530

    updated institution.csv

--- college_detail.py
+++ college_detail.py
@@ -1,22 +1,44 @@
-def college_table(session, cid, detail):
-    college_name = college_table.loc[college_table.cid==cid,'name'].values[0]
+def college_func(session, cid, detail):
+    row = college_table.loc[college_tabe.cid == cid]
+    college_name = row.name.value
+
+    if detail in ['photos   ','reviews']:
+        if str(row.place_id.value) == 'nan':
+            possible_chips = ['about', 'contact', 'address', 'reviews', 'photos'] - detail
+            chips = set(random.sample(possible_chips, 3))
+            make_response(jsonify({
+
+                'session' : session,
+
+                'messages' :[
+                    {'text': f"Sorry, I don't have any {detail} for {college_name}"},
+                    {'text': 'What else you want to know?'},
+                    {'chips': chips}
+                ]
+            }))
+            return make_json(session,{})
+
+        else:
+            url = "https://maps.googleapis.com/maps/api/place/details/json"
+
+            querystring = {"key":"AIzaSyAO7ow2BD4c3BmFonOUjVshoAgf_P5ZhYo","placeid":str(row.place_id.value)}
+
+            headers = {
+                'cache-control': "no-cache",
+                'postman-token': "fe1605ac-5a86-7b9f-4074-ca50dc58dbc5"
+            }
+            response = requests.request("POST", url, headers=headers, params=querystring)
+            response = response.json()
+
+
 
     if detail in ['address','photo','contact','reviews']:
         if cid not in google_table[1].values:
             if detail in ['photo','reviews']:
-                return make_json(session,{'text': f'Sorry, we don\'t have any {detail} for {college_name} })
+
             else:
                 # https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=&key=YOUR_API_KEY
 
-        url = "https://maps.googleapis.com/maps/api/place/details/json"
-
-        querystring = {"key":"AIzaSyAO7ow2BD4c3BmFonOUjVshoAgf_P5ZhYo","placeid":'ChIJM7k39sBPXDkRiU6Om4PD_e8'}
-
-        headers = {
-            'cache-control': "no-cache",
-            'postman-token': "fe1605ac-5a86-7b9f-4074-ca50dc58dbc5"
-        }
-        response = requests.request("POST", url, headers=headers, params=querystring)
 
         if detail == 'address':
 
